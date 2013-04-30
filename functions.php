e9839f2452615ca39976e93e55b0192cf2c6c747
David Stevens <david.stevens@zanebenefits.com>
2013-04-30 15:39:39 -0600

    added global functions and doT tempalting engine

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php // functions
+
+getTemplates() {
+	
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-require(["jquery","vendor/modernizr-2.6.2.min","plugins","navigation"], function($) {
+require(["jquery","vendor/modernizr-2.6.2.min","plugins","navigation","vendor/doT.min"], function($) {
     $(function() {
         //
     });
